2ea045a5e2ea0b96d3731fa923af91678891a365
Sabina Leitoiu <Sabina Leitoiu>
2017-04-12 00:06:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/royalteascafe/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.royalteascafe;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToMenu (View view) {
+       goToUrl("www.royalteascafe.co.uk/Breakfasts.html/");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+}
